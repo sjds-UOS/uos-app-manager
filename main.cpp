867a8d0d683a66e12d835aca23fc0451454f4271
xhd0728 <xinhaidong0625@foxmail.com>
2023-03-30 15:37:41 +0800

    获取应用列表并展示

--- main.cpp
+++ main.cpp
@@ -9,57 +9,82 @@
 #include <QPropertyAnimation>
 #include <QDate>
 #include <QLayout>
+//
+#include <QDir>
+#include <QStandardItemModel>
+#include <QListView>
+#include <QTableView>
 DWIDGET_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-    DApplication a(argc, argv);
-    a.setOrganizationName("deepin");
-    a.setApplicationName("dtk-application");
-    a.setApplicationVersion("1.0");
-    a.setProductIcon(QIcon(":/images/logo.svg"));
-    a.setProductName("Dtk Application");
-    a.setApplicationDescription("This is a dtk template application.");
+    DApplication app(argc, argv);
+        app.setOrganizationName("Harbin Engineering University");
+        app.setApplicationName("uos-appmanager");
+        app.setApplicationVersion("0.0.1");
+        app.setProductIcon(QIcon(":/images/logo.svg"));
+        app.setProductName("应用启动器");
+        app.setApplicationDescription("基于uos的开机自动启动的启动器");
+        // 提供中文界面支持
+        app.loadTranslator();
+        app.setApplicationDisplayName(QCoreApplication::translate("Main", "DTK Application"));
 
-    a.loadTranslator();
-    a.setApplicationDisplayName(QCoreApplication::translate("Main", "DTK Application"));
+        // 保存程序的窗口主题设置
+        DApplicationSettings as;
+        Q_UNUSED(as)
+        // 创建一个QStandardItemModel
+        QStandardItemModel *model = new QStandardItemModel();
+        model->setHorizontalHeaderLabels(QStringList() << "应用程序名称" << "操作");
+//        model->setHorizontalHeaderLabels(QStringList() << "Name" << "Icon");
+        // 获取所有的.desktop文件
+        QDir dir("/usr/share/applications");
+        // 有些应用程序不是以.desktop结尾的
+//        QStringList filters;
+//        filters << "*.desktop";
+//        dir.setNameFilters(filters);
+        QStringList files = dir.entryList();
+        // 解析每个.desktop文件，获取应用程序的名称和图标路径
+        foreach(QString file, files) {
+            QFile f("/usr/share/applications/" + file);
+            if(f.open(QIODevice::ReadOnly)) {
+                QTextStream in(&f);
+                QString line = in.readLine();
+                QString name, icon;
+                while(!line.isNull()) {
+                    if(line.startsWith("Name=")) {
+                        name = line.mid(5);
+                    } else if(line.startsWith("Icon=")) {
+//                        icon = line.mid(5);
+                    }
+                    line = in.readLine();
+                }
+                f.close();
+                // 将应用程序的名称和图标添加到QStandardItemModel中
+                // 图标的路径并不统一，待完善
+                if(!name.isEmpty()) {
+                    QStandardItem *item = new QStandardItem(name);
+                    if(!icon.isEmpty()) {
+                        item->setIcon(QIcon("/usr/share/icons/" + icon));
+                    }
+                    model->appendRow(item);
+                }
+            }
+        }
+        // 创建一个QTableView控件，并将QStandardItemModel绑定到该控件上
+        QTableView *tableView = new QTableView();
+        tableView->setModel(model);
+        tableView->setColumnWidth(0, 300);
+        // 将QTableView控件加入到DMainWindow中的中心部件中
+        DMainWindow window;
+        window.titlebar()->setIcon(QIcon(":/images/logo.svg"));
+        window.titlebar()->setTitle("应用启动器");
+        // 设置标题，宽度不够会隐藏标题文字
+        window.setMinimumSize(QSize(600, 200));
+        window.setGeometry(100, 100, 800, 600);
+        window.setCentralWidget(tableView);
+        window.show();
 
-    // 保存程序的窗口主题设置
-    DApplicationSettings as;
-    Q_UNUSED(as)
-
-    DMainWindow w;
-    w.titlebar()->setIcon(QIcon(":/images/logo.svg"));
-    w.titlebar()->setTitle("Hello dtk");
-    // 设置标题，宽度不够会隐藏标题文字
-    w.setMinimumSize(QSize(600, 200));
-
-    QWidget *cw = new QWidget(&w);
-    QVBoxLayout *layout = new QVBoxLayout(cw);
-    QDate today = QDate::currentDate();
-    DProgressBar *yearProgressBar = new DProgressBar();
-    yearProgressBar->setMaximum(today.daysInYear());
-    // 绑定字体大小
-    DFontSizeManager::instance()->bind(yearProgressBar, DFontSizeManager::T1);
-
-    yearProgressBar->setAlignment(Qt::AlignCenter);
-    QObject::connect(yearProgressBar, &DProgressBar::valueChanged, yearProgressBar, [yearProgressBar](int value){
-        yearProgressBar->setFormat(QString("您的 %1 使用进度： %2%").arg(QDate::currentDate().year())
-                                   .arg(value * 100 / yearProgressBar->maximum()));
-    });
-
-    layout->addWidget(yearProgressBar);
-    w.setCentralWidget(cw);
-    w.show();
-
-    auto animation = new QPropertyAnimation(yearProgressBar, "value");
-    animation->setDuration(5000);
-    animation->setStartValue(0);
-    animation->setEndValue(today.dayOfYear());
-    animation->start();
-
-    Dtk::Widget::moveToCenter(&w);
-
-    return a.exec();
+        Dtk::Widget::moveToCenter(&window);
+        return app.exec();
 }
--- uos_app_manager.pro
+++ uos_app_manager.pro
@@ -1,4 +1,5 @@
-QT += core gui dtkwidget
+QT += core gui
+QT += dtkwidget dtkgui dtkcore
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
